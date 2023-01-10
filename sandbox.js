071ba8bd1d1e83e25816456c5a58f803d2a13839
gbbgd3 <williambrar6@gmail.com>
2023-01-10 15:33:52 -0600

    working with strings

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,37 @@
-console.log('hello world');
+// let age = 25;
+// let year = 2022;
+
+// //log things to console
+// console.log(age, year);
+
+// age = 30;
+
+// console.log(age);
+
+// const point = 100;
+// console.log(point);
+
+// var score = 75;
+// console.log(score);
+
+//strings
+console.log('hello, world');
+
+let email = 'mario@thenetninja.co.uk';
+console.log(email);
+
+//string concat
+let firstName = 'Brandon';
+let lastName = 'Sanderson';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+//getting characters
+console.log(fullName[0]);
+
+//string length
+console.log(fullName.length);
+
+//string methods
+console.log(fullName.toUpperCase());
