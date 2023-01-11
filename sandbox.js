ad200c75c849fe7bddfcecaf78a780b0fe45fcf4
gbbgd3 <williambrar6@gmail.com>
2023-01-11 08:42:22 -0600

    template literals

--- sandbox.js
+++ sandbox.js
@@ -69,4 +69,18 @@ let likes = 10;
 
 likes %= 3;
 
-console.log(likes);
+const title = "Best reads of 2019";
+const author = "Mario";
+
+let result =`The blog called ${title} by ${author} has ${likes} likes`
+
+console.log(result);
+
+//creating html template
+let html = `
+        <h2>${title}</h2>
+        <p>By ${author}</p>
+        <span>This blog has ${likes} likes</span>
+`;
+
+console.log(html);
